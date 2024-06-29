bd0db7b02d16dfb5c75f249d00d3b2ce327d719e
Srinivas Manikanta <97425800+S-1729@users.noreply.github.com>
2024-06-29 14:41:38 +0530

    Add files via upload

--- Longest Palindromic Subsequence.cpp
+++ Longest Palindromic Subsequence.cpp
@@ -21,6 +21,28 @@ public:
     }
 };
 
+//Bottom-Up(Memoization)
+class Solution {
+public:
+    int dp[1001][1001];
+    int solve(string s,int i,int j){
+        if(i>j)
+            return 0;
+        if(i==j)
+            return 1;
+        if(dp[i][j]!=-1)
+            return dp[i][j];
+        if(s[i]==s[j])
+            return dp[i][j]=2+solve(s,i+1,j-1);
+        else
+            return dp[i][j]=max(solve(s,i+1,j),solve(s,i,j-1));
+    }
+    int longestPalindromeSubseq(string s) {
+        memset(dp,-1,sizeof dp);
+        return solve(s,0,s.size()-1);
+    }
+};
+
 /*
     Given a string s, find the longest palindromic subsequence's length in s.   
     A subsequence is a sequence that can be derived from another sequence by deleting some or no elements without changing the order of the remaining elements.
