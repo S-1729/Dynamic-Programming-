66635a07bad2d6fa5d1c775eb00dec86bb2ac224
Srinivas Manikanta <97425800+S-1729@users.noreply.github.com>
2024-07-18 20:13:39 +0530

    Add files via upload

--- Longest alternating subsequence.cpp
+++ Longest alternating subsequence.cpp
@@ -1,6 +1,21 @@
 class Solution {
   public:
     int alternatingMaxLength(vector<int>& arr) {
+        int ups=1,downs=1;
+        for(int i=1;i<arr.size();i++){
+            if(arr[i]>arr[i-1])
+                ups=downs+1;
+            if(arr[i]<arr[i-1])
+                downs=ups+1;
+        }
+        return max(ups,downs);
+    }
+};
+
+//Code-2
+class Solution {
+  public:
+    int alternatingMaxLength(vector<int>& arr) {
         if(arr.size()==1)
         return 1;
         if(arr.size()==2){
