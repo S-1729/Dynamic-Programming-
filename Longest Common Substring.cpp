161939547ab790d433de18780bea932dfe762ed3
Srinivas Manikanta <97425800+S-1729@users.noreply.github.com>
2024-08-15 10:36:05 +0530

    Add files via upload

--- Longest Common Substring.cpp
+++ Longest Common Substring.cpp
@@ -1,3 +1,30 @@
+//Top-Down
+class Solution {
+  public:
+    int ans=0;
+    int dp[1001][1001];
+    int solve(string str1,string str2,int i,int j){
+        if(i>=str1.size() || j>=str2.size())
+            return 0;
+        if(dp[i][j]!=-1)
+            return dp[i][j];
+        int res=0;
+        if(str1[i]==str2[j]){
+            res=1+solve(str1,str2,i+1,j+1);
+            ans=max(ans,res);
+        }
+        solve(str1,str2,i,j+1);
+        solve(str1,str2,i+1,j);
+        return dp[i][j]=res;
+    }
+    int longestCommonSubstr(string str1, string str2) {
+        memset(dp,-1,sizeof dp);
+        solve(str1,str2,0,0);
+        return ans;
+    }
+};
+
+
 //Bottom-Up
 class Solution {
   public:
